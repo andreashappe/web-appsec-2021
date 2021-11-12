182783127cffe6c98d398d60d9c1fb5a3c7145dd
Andreas Happe <andreashappe@snikt.net>
2021-11-12 14:01:55 +0100

    add ejs + layouts + helmet

--- app.mjs
+++ app.mjs
@@ -2,11 +2,20 @@ import express from "express"
 import dotenv from "dotenv";
 import PostsService from "./services/posts_service.mjs";
 import PostsStorageMemory from "./models/posts_storage_memory.mjs";
+import expressEjsLayouts from "express-ejs-layouts";
+import helmet from "helmet";
 
 // load potential config data from .env file
 dotenv.config()
 
 const app = express();
+
+/* setup ejs as view engine */
+app.set('view engine', 'ejs');
+app.use(expressEjsLayouts);
+app.set('layout', 'layouts/default');
+
+app.use(helmet());
 
 const postStorage = new PostsStorageMemory();
 const postsService = new PostsService(postStorage);
@@ -19,14 +28,14 @@ app.get('/', function(req, res) {
 });
 
 app.get('/posts', function(req, res) {
-    res.send(postsService.listPosts())
+    res.render("posts/index.ejs", {posts: postsService.listPosts() });
 });
 
 app.get('/posts/:id', function(req, res) {
     let post = postsService.getPost(parseInt(req.params.id));
 
     if (post) {
-        res.send(post);
+        res.render("posts/show.ejs", {post: post });
     } else {
         res.status(404).send("not found");
     }
